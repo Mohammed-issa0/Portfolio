d820fe6065e56dc323a273e63481e79e51b2eb60
Mohammed-issa0 <mohammed.issaopte@gmail.com>
2024-07-15 23:42:50 +0300

    responsive done

--- script.js
+++ script.js
@@ -60,3 +60,39 @@ circles.forEach(elem=>{
 
 // mix it up portofoli 
 var mixer = mixitup('.portofolio-gallery');
+
+// active menu------------------------------------------------
+let menuLi= document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){
+    }
+    menuLi.forEach(sec => sec.classList.remove('active'));
+    menuLi[len].classList.add('active');
+
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+// sticky navbar--------------------------------------
+const header = document.querySelectorAll("header");
+window.addEventListener("scroll" , function(){
+    header.classList.toggle("sticky", window.scrollY > 50)
+})
+
+// toggle navbar icon---------------------------------
+let menuIcon = document.querySelector("#menu-icon");
+let navlise = document.querySelector(".navlist");
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlise.classList.toggle("open");
+}
+
+window.onscroll =()=>{
+    menuIcon.classList.remove("bx-x");
+    navlise.classList.replace("open");
+}
